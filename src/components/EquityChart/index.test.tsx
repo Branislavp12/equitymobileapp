aac171e83aeafb1950a78d0ed91e74b4215be612
Branislavp12 <115873169+Branislavp12@users.noreply.github.com>
2025-08-14 22:52:42 +0200

    Configure Jest for d3 ESM modules

--- src/components/EquityChart/index.test.tsx
+++ src/components/EquityChart/index.test.tsx
@@ -1,25 +1,39 @@
-import { describe, it, expect } from "@jest/globals";
+import { describe, it, expect, beforeAll } from "@jest/globals";
 import React from 'react';
-import renderer from 'react-test-renderer';
+import renderer, { act } from 'react-test-renderer';
 
 import EquityChart from './index';
 import ChartAxes from './ChartAxes';
 import ChartHistoryLine from './ChartHistoryLine';
 
+beforeAll(() => {
+  // Disable development logs during tests
+  // to avoid "Cannot log after tests are done" errors.
+  // @ts-ignore
+  global.__DEV__ = false;
+});
+
 describe('EquityChart component', () => {
   it('renders nothing when history is empty', () => {
-    const tree = renderer
-      .create(<EquityChart history={[]} initialCapital={1000} />)
-      .toJSON();
+    let tree: renderer.ReactTestRendererJSON | renderer.ReactTestRendererJSON[] | null;
+    act(() => {
+      tree = renderer
+        .create(<EquityChart history={[]} initialCapital={1000} />)
+        .toJSON();
+    });
     expect(tree).toBeNull();
   });
 
   it('renders axes and history line for provided history', () => {
-    const component = renderer.create(
-      <EquityChart history={[100, 105, 110]} initialCapital={1000} />
-    );
+    let component: renderer.ReactTestRenderer;
+    act(() => {
+      component = renderer.create(
+        <EquityChart history={[100, 105, 110]} initialCapital={1000} />
+      );
+    });
 
-    expect(() => component.root.findByType(ChartAxes)).not.toThrow();
-    expect(() => component.root.findByType(ChartHistoryLine)).not.toThrow();
+    expect(component.root.findByType(ChartAxes)).toBeTruthy();
+    expect(component.root.findByType(ChartHistoryLine)).toBeTruthy();
   });
 });
+
